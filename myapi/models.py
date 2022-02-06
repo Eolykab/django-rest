566b33d5a12c6f557ff2ff1210e3d2eaa1783931
Eljoenai Muninga <eolykab@yahoo.co.za>
2022-02-06 18:07:59 +0200

    Added models and migrations

--- myapi/models.py
+++ myapi/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Installation(models.Model):
+    customer_name = models.CharField(max_length=60)
+    address = models.CharField(max_length=255)
+    appointment_date = models.DateField()
+    date_created = models.DateField(default= date.today)
+    date_modified = models.DateField(default= date.today)
+
+
+    def __str__(self):
+        return self.customer_name
+
+
+
+class Status(models.Model):
+
+    class StatusIndication (models.Choices):
+        REQUESTED = 'Requested'
+        IN_PROGRESS = 'In Progress'
+        COMPLETE = 'Complete'
+        REJECTED = 'Rejected'
+
+    installation = models.ForeignKey(Installation, on_delete=models.CASCADE)
+    status = models.CharField(max_length = 20, choices= StatusIndication.choices, default=StatusIndication.REQUESTED)
+    notes = models.TextField()
+    date = models.DateField()
+
+
+    def __str__(self):
+        return self.status
